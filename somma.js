6dacac38875117688d21a5d25b4043a6990104bc
federicoSetti <federicosetti98@gmail.com>
2016-10-24 18:13:15 +0200

    aggiunto es. somma

---
+++ somma.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function somma(a,b) 
+{
+    somma = a + b;
+    console.log(somma);
+}
+
+const x = function somma1 (a, b)
+{
+    somma1 = a + b;
+    console.log(somma1);
+}
+
+const y = (a,b) => {
+    somma2 = a + b;
+    console.log(somma2);
+                   }
